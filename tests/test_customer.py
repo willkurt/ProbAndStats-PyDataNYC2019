09fab43662f913aac510d0874f7f1e4c0cd91a3d
Will Kurt <wckurt@gmail.com>
2019-10-20 14:04:55 -0400

    added basic testing structure

---
+++ tests/test_customer.py
@@ -0,0 +1,14 @@
+import unittest
+
+from context import src
+from src import customer
+from src import product
+
+class TestCustomer(unittest.TestCase):
+
+    def test_example(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()

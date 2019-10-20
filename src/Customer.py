396bd2794626434cf10473fea74cb0cbd3d1c65c
Will Kurt <wckurt@gmail.com>
2019-10-19 22:28:38 -0400

    added basic implementation of a customer

---
+++ src/Customer.py
@@ -0,0 +1,15 @@
+class Customer:
+
+    def __init__(self,
+                 price_threshold,
+                 quality_threshold):
+        self.price_threshold = price_threshold
+        self.quality_threshold = quality_threshold
+
+    def __str__(self):
+        return "todo"
+
+    def will_purchase(product):
+        return ( product.price <= self.price_threshold and
+                 produce.quality >= self.quality_threshold)
+             
